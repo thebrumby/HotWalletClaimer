dcc93996f57cc141199f7c7c3e4d6777efea59ec
thebrumby <philip.stockdale@yahoo.com>
2024-05-20 21:47:10 +0100

    Update hot.py

--- games/hot.py
+++ games/hot.py
@@ -1048,11 +1048,11 @@ def start_pm2_app(script_path, app_name, session_name):
     subprocess.run(command, shell=True, check=True)
 
 # List all PM2 processes
-def save_pm2():
-    command = f"NODE_NO_WARNINGS=1 pm2 save"
-    result = subprocess.run(command, shell=True, text=True, capture_output=True)
-    print(result.stdout)
-
+def start_pm2_app(script_path, app_name, session_name):
+    interpreter_path = "venv/bin/python3" 
+    command = f"NODE_NO_WARNINGS=1 pm2 start {script_path} --name {app_name} --interpreter {interpreter_path} --watch {script_path} -- {session_name}"
+    subprocess.run(command, shell=True, check=True)
+    
 def backup_telegram():
     global session_path, step
 
