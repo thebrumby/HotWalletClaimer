cd616434dec229e7f0738ad01b496d4e7372fbbc
thebrumby <philip.stockdale@yahoo.com>
2024-05-21 01:38:35 +0100

    Update wave.py

--- games/wave.py
+++ games/wave.py
@@ -1052,10 +1052,10 @@ def validate_seed_phrase():
 # Start a new PM2 process
 def start_pm2_app(script_path, app_name, session_name):
     interpreter_path = "venv/bin/python3"
-    command = f"NODE_NO_WARNINGS=1 pm2 start {script_path} --name {app_name} --interpreter {interpreter_path} {session_name} --watch -- {script_path}"
+    command = f"NODE_NO_WARNINGS=1 pm2 start {script_path} --name {app_name} --interpreter {interpreter_path} --watch {script_path} -- {session_name}"
     subprocess.run(command, shell=True, check=True)
 
-# List all PM2 processes
+# Save the new PM2 process
 def save_pm2():
     command = f"NODE_NO_WARNINGS=1 pm2 save"
     result = subprocess.run(command, shell=True, text=True, capture_output=True)
