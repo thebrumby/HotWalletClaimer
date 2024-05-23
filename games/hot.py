b812a96d7724926c54779145f8262fcbce3c6969
thebrumby <philip.stockdale@yahoo.com>
2024-05-23 04:17:08 +0100

    Update hot.py

--- games/hot.py
+++ games/hot.py
@@ -22,6 +22,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException, ElementClickInterceptedException
 from datetime import datetime, timedelta
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def load_settings():
     global settings, settings_file
