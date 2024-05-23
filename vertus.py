59c43db487f52279d5fac594a2c602c0bc733f85
thebrumby <philip.stockdale@yahoo.com>
2024-05-23 15:19:01 +0100

    Update vertus.py

--- vertus.py
+++ vertus.py
@@ -66,7 +66,7 @@ load_settings()
 driver = None
 target_element = None
 random_offset = random.randint(settings['lowestClaimOffset'], settings['highestClaimOffset'])
-script = "games/vertus.py"
+script = "vertus.py"
 prefix = "Vertus:"
 url = "https://web.telegram.org/k/#@vertus_app_bot"
 
