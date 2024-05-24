818335156024b0424aebce60c209df4e761669ca
thebrumby <philip.stockdale@yahoo.com>
2024-05-24 19:47:23 +0100

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -22,12 +22,12 @@ run_script() {
 
 # Function to list and choose a script
 list_and_choose_script() {
-    echo "Listing available Telegram Claim Assistant scripts:"
-    # List all Python scripts in the current directory
-    IFS=$'\n' read -d '' -r -a scripts < <(find . -maxdepth 1 -name "*.py" -print && printf '\0')
+    echo "Listing available scripts in the current directory and ./games folder:"
+    # List all Python scripts in the current directory and ./games directory
+    IFS=$'\n' read -d '' -r -a scripts < <(find . ./games -maxdepth 1 -name "*.py" -print && printf '\0')
     
     if [ ${#scripts[@]} -eq 0 ]; then
-        echo "No Python scripts found in the current directory."
+        echo "No Python scripts found in the current directory or ./games directory."
         exit 1
     fi
 
@@ -43,7 +43,7 @@ list_and_choose_script() {
     
     if [ -n "$selected_script" ]; then
         activate_venv
-        run_script "$selected_script"
+        run_script "$selected_script" "${@:2}"
         deactivate_venv
     else
         echo "Invalid selection. Exiting..."
@@ -61,13 +61,18 @@ else
         script_name="${script_name}.py"
     fi
 
-    # Check if the script exists in the current directory
-    if [ -f "$script_name" ]; then
-        activate_venv
-        run_script "$script_name" "${@:2}"
-        deactivate_venv
-    else
-        echo "Specified script not found."
+    # Check if the script exists in the ./games directory or current directory
+    if [ -f "./games/$script_name" ]; then
+        script_path="./games/$script_name"
+    elif [ -f "$script_name" ]; then
+        script_path="$script_name"
+    else:
+        echo "Specified script not found in the ./games directory or current directory."
         list_and_choose_script
+        exit 1
     fi
+
+    activate_venv
+    run_script "$script_path" "${@:2}"
+    deactivate_venv
 fi
