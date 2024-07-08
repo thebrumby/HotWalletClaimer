dfd989ae168ce2b212241d144019986a60a98f1d
thebrumby <philip.stockdale@yahoo.com>
2024-07-08 21:46:49 +0100

    Update pm2.py

--- games/utils/pm2.py
+++ games/utils/pm2.py
@@ -4,11 +4,18 @@ import sys
 
 def start_pm2_app(script_path, app_name, session_name):
     interpreter_path = "venv/bin/python3"
-    command = f"NODE_NO_WARNINGS=1 pm2 start {script_path} --name {app_name} --interpreter {interpreter_path} --watch {script_path} -- {session_name}"
+    command = (
+        f"NODE_NO_WARNINGS=1 pm2 start {script_path} "
+        f"--name {app_name} "
+        f"--interpreter {interpreter_path} "
+        f"--watch {script_path} "
+        f"--output /dev/null "  # Redirect stdout to /dev/null
+        f"--error {app_name}_error.log "  # Log stderr to a specific file
+        f"-- {session_name}"
+    )
     subprocess.run(command, shell=True, check=True)
 
-# Save the new PM2 process
 def save_pm2():
-    command = f"NODE_NO_WARNINGS=1 pm2 save"
+    command = "NODE_NO_WARNINGS=1 pm2 save"
     result = subprocess.run(command, shell=True, text=True, capture_output=True)
     print(result.stdout)
