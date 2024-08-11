136ebd86b1b2089193a5901315ee91b19ea2b428
rostamzad1900 <rostamzad1900@gmail.com>
2024-08-12 01:04:01 +0330

    Daily Bonus Claim

--- games/seed.py
+++ games/seed.py
@@ -89,7 +89,7 @@ class SeedClaimer(Claimer):
         xpath = "//img[contains(@src,'inventory/worm')]"
         self.move_and_click(xpath, 20, True, "check for WORM (may not be present)", self.step, "clickable")
 
-        xpath = "//button[.//p[contains(text(), 'Sell now')]]"
+        xpath = "//button[.//p[contains(text(), 'Yep')]]"
         self.move_and_click(xpath, 20, True, "sell WORM (may not be present)", self.step, "clickable")
 
         # Get egg
@@ -97,7 +97,7 @@ class SeedClaimer(Claimer):
         self.move_and_click(xpath, 20, True, "check for EGG (may not be present)", self.step, "clickable")
         
         # GET DAILY BONUS
-        xpath = "//button[.//img[contains(@src, 'daily')]]"
+        xpath = "//button[.//img[contains(@src, 'fire')]]"
         self.move_and_click(xpath, 20, True, "check for DAILY BONUS (may not be present)", self.step, "clickable")
 
         xpath = "//button[count(.//img) = 1 and .//img[contains(@src, 'daily/')]]"
@@ -105,6 +105,9 @@ class SeedClaimer(Claimer):
 
         xpath = "//button[contains(text(), 'Got it')]"
         self.move_and_click(xpath, 20, True, "exit DAILY BONUS (may not be present)", self.step, "clickable")
+
+        xpath = "//div[button[contains(text(), 'Claim ticket')]]"
+        self.move_and_click(xpath, 20, True, "get Ticket (may not be present)", self.step, "clickable")
 
         self.get_balance(False)
         self.get_profit_hour(False)
