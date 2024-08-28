2021ad3cc30e9baedf523e1539a7a9cb2d4091b3
thebrumby <philip.stockdale@yahoo.com>
2024-08-28 21:49:41 +0100

    Update oxygen.py

--- games/oxygen.py
+++ games/oxygen.py
@@ -50,6 +50,23 @@ class OxygenClaimer(Claimer):
         self.random_offset = random.randint(self.settings['lowestClaimOffset'], self.settings['highestClaimOffset'])
         super().__init__()
 
+    def next_steps(self):
+        if self.step:
+            pass
+        else:
+            self.step = "01"
+
+        try:
+            self.launch_iframe()
+            self.increase_step()
+            self.set_cookies()
+
+        except TimeoutException:
+            self.output(f"Step {self.step} - Failed to find or switch to the iframe within the timeout period.", 1)
+
+        except Exception as e:
+            self.output(f"Step {self.step} - An error occurred: {e}", 1)
+
     def full_claim(self):
         self.step = "100"
 
