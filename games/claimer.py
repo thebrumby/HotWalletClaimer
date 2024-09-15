0bee5ca7777e4eef41420ce02082eeb6337679d7
thebrumby <philip.stockdale@yahoo.com>
2024-09-15 14:18:48 +0100

    Update claimer.py

--- games/claimer.py
+++ games/claimer.py
@@ -1426,7 +1426,7 @@ class Claimer:
                 self.random_offset = random.randint(low, high)
                 modifiedTimer = unmodifiedTimer + self.random_offset
                 self.output(
-                    f"Step {self.step} - Random offset applied to the wait timer of: {self.random_offset} minutes to original time of {unmodifiedTimer} ({format_time(unmodifiedTimer)}).",
+                    f"Step {self.step} - Random offset applied the random offset of: {self.random_offset} minutes to original time of {unmodifiedTimer} minutes.",
                     3
                 )
                 self.output(
