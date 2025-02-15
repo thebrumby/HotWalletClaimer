eddc4a877f64313d2dbf300fc260b6d4f339d18d
thebrumby <philip.stockdale@yahoo.com>
2025-02-15 21:22:48 +0000

    Update seed.py

--- games/seed.py
+++ games/seed.py
@@ -41,7 +41,7 @@ class SeedClaimer(Claimer):
         self.forceRequestUserAgent = False
         self.allow_early_claim = False
         self.start_app_xpath = "//button[descendant::span[contains(text(), 'Play')]]"
-        self.start_app_menu_item = "//a[@data-peer-id='6508172553']"
+        self.start_app_menu_item = "//a[.//span[contains(@class, 'peer-title') and normalize-space(text())='SEED']]"
 
     def __init__(self):
         self.settings_file = "variables.txt"
